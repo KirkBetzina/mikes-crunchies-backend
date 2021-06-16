fe1413f6f87a62ddbeb7ce40d0b1f79bdcc19b25
kirk <kirk.betzina@gmail.com>
2021-06-16 19:21:48 -0400

    finished crud functionality for the db in python

--- models/PYMONGO/databaseOperations.py
+++ models/PYMONGO/databaseOperations.py
@@ -484,7 +484,18 @@ def update_or_create(document_id, data):
     document = collection.update_one({'_id':ObjectId(document_id)},{'$set':data},upsert=True)
     return document.acknowledged
 
-   
+def get_single_data(document_id):
+    data = collection.find_one({'_id':ObjectId(document_id)})
+    return data    
+
+def get_multiple_data():
+    data = collection.find
+    return list(data)
+
+def update_existing(document_id, data):
+    document = collection.update_one({'_id':ObjectId(document_id)}, {'$set', data})
+    return document.acknowledged    
+
 
 def remove_data(document_id):
     document = collection.delete_one({'_id':ObjectId(document_id)})
